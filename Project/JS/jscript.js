12c28564227f5c9cd95bcd331955969a84e8ffe8
yousafshah10 <akyshahyo@gmail.com>
2023-03-14 17:44:43 -0400

    Merge branch 'Dev' of https://github.com/bit-yottabyte/COSC-4P02-Project into Yousaf

--- Project/JS/jscript.js
+++ Project/JS/jscript.js
@@ -1,77 +1,76 @@
-document.addEventListener("DOMContentLoaded", function() {
+
+document.addEventListener("DOMContentLoaded", function () {
 	renderNavBar();
 	renderBackground();
 });
 
-var renderNavBar = function() {
+var renderNavBar = function () {
 	var nav = document.createElement("nav");
 	nav.classList.add("navbar", "navbar-expand-md", "navbar-dark", "bg-dark");
-	
+
 	var div = document.createElement("div");
 	div.classList.add("navbar-collapse");
 	nav.appendChild(div);
-	
+
 	var ul = document.createElement("ul");
-	ul.classList.add("navbar-nav", "nav-fill", "w-100", "mr-auto", "mt-2", "mt-lg-0");
+	ul.classList.add(
+		"navbar-nav",
+		"nav-fill",
+		"w-100",
+		"mr-auto",
+		"mt-2",
+		"mt-lg-0"
+	);
 	div.appendChild(ul);
-	
+
 	//home nav button
 	var liHome = document.createElement("li");
 	liHome.classList.add("nav-item");
-	
+
 	var aHome = document.createElement("a");
 	aHome.classList.add("nav-link");
 	aHome.innerHTML = "Home";
-	aHome.href = "#";
-	
+	aHome.href = "index.html";
+
 	//interactive timeline nav button
 	var liTimeline = document.createElement("li");
 	liTimeline.classList.add("nav-item");
-	
+
 	var aTimeline = document.createElement("a");
 	aTimeline.classList.add("nav-link");
 	aTimeline.innerHTML = "Interactive Timeline";
-	aTimeline.href = "#";
-	
-	//interactive timeline nav button
-	var liTimeline = document.createElement("li");
-	liTimeline.classList.add("nav-item");
-	
-	var aTimeline = document.createElement("a");
-	aTimeline.classList.add("nav-link");
-	aTimeline.innerHTML = "Interactive Timeline";
-	aTimeline.href = "#";
-	
-	//events timeline nav button
+	aTimeline.href = "timeline.html";
+
+	//events nav button
 	var liEvents = document.createElement("li");
 	liEvents.classList.add("nav-item");
-	
+
 	var aEvents = document.createElement("a");
 	aEvents.classList.add("nav-link");
 	aEvents.innerHTML = "Events";
-	aEvents.href = "#";
-	
-	//questionnaire timeline nav button
+	aEvents.href = "events.html";
+
+	//questionnaire nav button
 	var liQuestion = document.createElement("li");
 	liQuestion.classList.add("nav-item");
-	
+
 	var aQuestion = document.createElement("a");
 	aQuestion.classList.add("nav-link");
 	aQuestion.innerHTML = "Questionnaire";
-	aQuestion.href = "#";
-	
-	//quiz timeline nav button
+	aQuestion.href = "questionnaire.html";
+
+	//quiz nav button
 	var liQuiz = document.createElement("li");
 	liQuiz.classList.add("nav-item");
-	
+
 	var aQuiz = document.createElement("a");
 	aQuiz.classList.add("nav-link");
 	aQuiz.innerHTML = "Quiz";
-	aQuiz.href = "#";
-	
+	aQuiz.href = "quiz.html";
+
 	//assign the correct active list item in the navbar
 	var pageName = getPageName();
-	switch(pageName) {
+	switch (pageName) {
 		case "home":
 			aHome.classList.add("active");
 			aHome.setAttribute("style", "border-bottom: 2px solid #4287f5");
@@ -93,30 +92,33 @@ var renderNavBar = function() {
 			aQuiz.setAttribute("style", "border-bottom: 2px solid #4287f5");
 			break;
 	}
-	
+
 	//add the created elements to the navbar element
 	ul.appendChild(liHome);
 	liHome.appendChild(aHome);
 	ul.appendChild(liTimeline);
 	liTimeline.appendChild(aTimeline);
 	ul.appendChild(liEvents);
-	liEvents.appendChild(aEvents);	
+	liEvents.appendChild(aEvents);
 	ul.appendChild(liQuestion);
 	liQuestion.appendChild(aQuestion);
 	ul.appendChild(liQuiz);
 	liQuiz.appendChild(aQuiz);
-	
+
 	document.body.insertBefore(nav, document.body.firstChild);
-	
+
 	//must do this after adding elements to the DOM so we can modify them through fetching them from DOM
 	var liItems = document.getElementsByTagName("li");
-	for(var i = 0; i < liItems.length; i++) {	
-		liItems[i].setAttribute("style", "box-shadow: inset 0 0 10px #6b7482; border-radius: 4px; margin-right: 1px; margin-left: 1px; padding-right: 1px; padding-left: 1px");
+	for (var i = 0; i < liItems.length; i++) {
+		liItems[i].setAttribute(
+			"style",
+			"box-shadow: inset 0 0 10px #6b7482; border-radius: 4px; margin-right: 1px; margin-left: 1px; padding-right: 1px; padding-left: 1px"
+		);
 		liItems[i].classList.add("col-12");
 	}
 };
 
-var renderBackground = function() {
+var renderBackground = function () {
 	document.body.setAttribute("style", "overflow-x: hidden");
 	document.body.style.backgroundImage = "url('images/museum.png')";
 	document.body.style.backgroundSize = "100vw 100vh";
@@ -124,18 +126,18 @@ var renderBackground = function() {
 
 function getPageName() {
 	url = window.location.href;
-	if(!url || (url && url.length === 0)) {
+	if (!url || (url && url.length === 0)) {
 		return "";
 	}
-	
+
 	var index = url.lastIndexOf("/") + 1;
 	var filenameWithExtension = url.substr(index);
 	var basename = filenameWithExtension.split(/[.?&#]+/)[0];
 
-	if(basename.length === 0) {
-		url = url.substr(0,index-1);
+	if (basename.length === 0) {
+		url = url.substr(0, index - 1);
 		basename = getBaseName(url);
 	}
-	
-	return basename ? basename : ""; 
+
+	return basename ? basename : "";
 }
