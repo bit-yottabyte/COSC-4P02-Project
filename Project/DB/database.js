6a18898627515908e603b85bf2184dfa42ab5685
yousafshah10 <akyshahyo@gmail.com>
2023-03-23 20:35:05 -0400

    ..

--- Project/DB/database.js
+++ Project/DB/database.js
@@ -11,7 +11,6 @@ const Artifacts = require("../models/artifacts");
 const Location = require("../models/location");
 const local_museum = require("../models/localmuseum");
 const Quiz = require("../models/quiz");
-const Questionnaire = require("../models/questionnaire");
 
 const uri =
 	"mongodb+srv://" +
@@ -28,8 +27,8 @@ db.once("open", function () {
 	console.log("Database connected");
 });
 
-//endpoint to query the entire events collection
-app.post("/queryAllEvents", async (req, res) => {
+//endpoint to query the events collection
+app.post("/queryEvents", async (req, res) => {
 	try {
 		const events = await Events.find({});
 		res.json(events);
@@ -38,53 +37,10 @@ app.post("/queryAllEvents", async (req, res) => {
 	} finally {
 	}
 });
-app.use(express.urlencoded({ extended: true }));
 
-//endpoint to add to questionnaire collection
-app.post("/add", async (req, res) => {
-	try {
-		const newAnswer = new Questionnaire({
-			name: req.body.name,
-			email: req.body.email,
-			q3: req.body.dropdown,
-			q4: req.body.text,
-			q5: req.body.radio,
-		});
-		newAnswer.save();
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	} finally {
-	}
-});
-
-//endpoint to query the entire artifacts collection
-app.post("/queryAllArtifacts", async (req, res) => {
-	try {
-		const artifacts = await Artifacts.find({});
-		res.json(artifacts);
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	} finally {
-	}
-});
-
-//endpoint to query the artifacts collection based on matching name input
+//endpoint to query the artifacts collection
 app.post("/queryArtifacts", async (req, res) => {
 	try {
-		//find 10 most similar artifacts by matching name
-		const artifacts = await Artifacts.find({
-			name: { $regex: req.query.name, $options: "i" },
-		}).limit(10);
-		res.json(artifacts);
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	} finally {
-	}
-});
-
-//endpoint to query the artifacts collection based on matching name input
-app.post("/queryArtifactByID", async (req, res) => {
-	try {
 		const artifacts = await Artifacts.find({});
 		res.json(artifacts);
 	} catch (error) {
@@ -92,7 +48,6 @@ app.post("/queryArtifactByID", async (req, res) => {
 	} finally {
 	}
 });
-
 //endpoint to query the location collection
 app.post("/queryLocation", async (req, res) => {
 	try {
@@ -103,7 +58,6 @@ app.post("/queryLocation", async (req, res) => {
 	} finally {
 	}
 });
-
 //endpoint to query the local museum collection
 app.post("/queryLocalMuseum", async (req, res) => {
 	try {
@@ -114,12 +68,10 @@ app.post("/queryLocalMuseum", async (req, res) => {
 	} finally {
 	}
 });
-
 //endpoint to query the quiz collection
 app.post("/queryQuiz", async (req, res) => {
 	try {
-		//query 15 quiz questions
-		const quiz = await Quiz.find({}).limit(15);
+		const quiz = await Quiz.find({});
 		res.json(quiz);
 	} catch (error) {
 		res.status(500).json({ message: error.message });
