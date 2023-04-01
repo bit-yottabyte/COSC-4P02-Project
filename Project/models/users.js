9367a7bf6e0e9778d6c7645c43a413cefacc48d9
yathy1040 <tyathusan26@gmail.com>
2023-04-01 14:35:06 -0400

    doing login backend

--- Project/models/users.js
+++ Project/models/users.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-	username: String,
+	uname: String,
+	usid_1: Number,
 	password: String,
 });
-
-//third parameter explicitly names the collection reference to "quiz" instead of "quizzes"
-const User = mongoose.model("users", userSchema);
-
 // hash the password
 userSchema.methods.generateHash = function (password) {
 	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
@@ -17,5 +15,8 @@ userSchema.methods.generateHash = function (password) {
 userSchema.methods.validPassword = function (password) {
 	return bcrypt.compareSync(password, this.password);
 };
+
+//third parameter explicitly names the collection reference to "quiz" instead of "quizzes"
+const User = mongoose.model("users", userSchema);
 
 module.exports = User;
